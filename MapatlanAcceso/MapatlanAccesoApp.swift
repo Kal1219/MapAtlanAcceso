664c53fcc7e3f78abf97f73a998a280186838fbc
Kal1219 <kaleb.cortes1208@gmail.com>
2023-08-29 14:15:08 -0600

    Initial Commit

---
+++ MapatlanAcceso/MapatlanAccesoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapatlanAccesoApp.swift
+//  MapatlanAcceso
+//
+//  Created by Kaleb on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MapatlanAccesoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
